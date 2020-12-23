47c9c82a0fe97d17daa0ba33f84f87c094d58d67
pgm-ryanjudd <ryanjudd@student.artveldehs.be>
2020-12-23 11:23:36 +0100

    fetch in services works

--- static/js/main.js
+++ static/js/main.js
@@ -1,72 +1,80 @@
-// const WEATHER_API = 'http://api.weatherapi.com/v1/current.json?key=4f49bbe7048d41d7b12142024202112&q=Ghent';
+const WEATHER_API = 'http://api.weatherapi.com/v1/current.json?key=4f49bbe7048d41d7b12142024202112&q=Ghent';
 const COVID_API = 'https://data.stad.gent/api/records/1.0/search/?dataset=dataset-of-cumulative-number-of-confirmed-cases-by-municipality&q=';
 
+// test = WeatherApi().getCurrentWeather();
+// console.log(test); // undefined
+
 (() => {
-    const app = {
-        initialize() {
-            this.cacheElements();
-            this.fetchGhentCovidPositiveCases();
-            this.fetchGHUsers();
-            this.test();
-        },
+  const app = {
+    initialize() {
+      this.cacheElements();
+      this.fetchGhentCovidPositiveCases();
+      this.fetchGHUsers();
+      this.WeatherFromServices();
+    },
 
-        cacheElements() {
-            this.$weatherContainer = document.querySelector('.weather-container');
-            this.$covidContainer = document.querySelector('.covid-container');
-            this.$githubContainer = document.querySelector('.github-container')
-        },
+    cacheElements() {
+      this.$weatherContainer = document.querySelector('.weather-container');
+      this.$covidContainer = document.querySelector('.covid-container');
+      this.$githubContainer = document.querySelector('.github-container');
+    },
 
-        test(json) {
-            // console.log(hey);
-            console.log(WeatherApi().getCurrentWeather());
-            this.updateWeather(WeatherApi());
-            console.log(this.getCurrentWeather);
-            
-            
-            // console.log(test);
-        },
+    async WeatherFromServices() {
+      // console.log(hey);
+      // console.log(test.current.temp_c);
+      // console.log(test.getCurrentWeather());
+      // fetch(WEATHER_API)
+      //   .then(response => response.json())
+      //   .then(data => this.updateWeather(data));
 
-        updateWeather(weather) {
-            this.$weatherContainer.innerHTML = `<h3>${weather.current.temp_c} °C</h3>`;
-        },
+      const ei = await WeatherApi();
 
-        async fetchGhentCovidPositiveCases() {
-            try {
-                const response = await fetch(COVID_API);
-                const json = await response.json();
-                this.updateGhentCovidPositiveCases(json);
-            } catch (error) {
-                console.error(error);
-            }
-        },
+      this.updateWeather(ei)
 
-        updateGhentCovidPositiveCases: function (cases) {
-            this.$covidContainer.innerHTML = `<h3>${cases.records[0].fields.cases}</h3>`
-        },
+      let b = 5;
+      let c = simpleMath()+b;
+      console.log(c);
+    },
 
-        async fetchGHUsers() {
-            try {
-                const response = await fetch('./static/data/pgm.json');
-                const json = await response.json();
-                this.updateGHUsersList(json);
-            } catch (error) {
-                console.error(error);
-            }
-        },
+    updateWeather(weather) {
+      console.log(weather);
+      this.$weatherContainer.innerHTML = `<h3>${weather.current.temp_c} °C</h3> <img src="${weather.current.condition.icon}" alt="">`;
+    },
 
-        updateGHUsersList(users) {
-            let str = '';
+    async fetchGhentCovidPositiveCases() {
+      try {
+        const response = await fetch(COVID_API);
+        const json = await response.json();
+        this.updateGhentCovidPositiveCases(json);
+      } catch (error) {
+        console.error(error);
+      }
+    },
 
-            users.forEach(user => {
-                str += `
-                <p>${user.portfolio.githubUsername}</p>
-                <p>${user.name} ${user.familyName}</p>
+    updateGhentCovidPositiveCases: function (cases) {
+      this.$covidContainer.innerHTML = `<h3>${cases.records[0].fields.cases}</h3>`
+    },
 
-                `;
-            });
+    async fetchGHUsers() {
+      try {
+        const response = await fetch('./static/data/pgm.json');
+        const json = await response.json();
+        this.updateGHUsersList(json);
+      } catch (error) {
+        console.error(error);
+      }
+    },
 
-            this.$githubContainer.innerHTML = str;
-        }
+    updateGHUsersList(users) {
+      let str = '';
+      users.forEach(user => {
+        str += `
+        <p>${user.portfolio.githubUsername}</p>
+        <p>${user.name} ${user.familyName}</p>
+        `;
+      });
+      this.$githubContainer.innerHTML = str;
     }
-    app.initialize();
-  })()
+  }
+  app.initialize();
+})()
--- static/js/services.js
+++ static/js/services.js
@@ -1,19 +1,21 @@
-
-
-function WeatherApi() {
-
-  this.getCurrentWeather = async () => {
+async function WeatherApi() {
+  // this.getCurrentWeather = async () => {
     const WEATHER_API = 'http://api.weatherapi.com/v1/current.json?key=4f49bbe7048d41d7b12142024202112&q=Ghent';
-
-    const json = "";
 
     try {
       const response = await fetch(WEATHER_API);
       json = await response.json();
+      return json;
     } catch (error) {
       console.error(error);
     }
 
-    return json;
-  }
+}
+
+function simpleMath() {
+
+  let x = 5, y = 10, z = x+y;
+  
+  return z;
+  
 }
